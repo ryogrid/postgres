28620efc0c5d49e0b137940052f88a2b4ab70517
Ryo Kanbayashi <ryo.contact@gmail.com>
2025-01-19 14:36:34 +0900

    add cmd_out.c and checking with it worked.

--- src/interfaces/ecpg/test/preproc/cmd_out.c
+++ src/interfaces/ecpg/test/preproc/cmd_out.c
@@ -1,6 +1,6 @@
 /*-------------------------------------------------------------------------
  *
- * cmd_out_informix --- assistance program for ecpg syntax error detecting
+ * cmd_out          --- assistance program for ecpg syntax error detecting
  *                      with pg_regress
  * 
  * This code is released under the terms of the PostgreSQL License.
@@ -8,7 +8,7 @@
  * Portions Copyright (c) 1996-2025, PostgreSQL Global Development Group
  * Portions Copyright (c) 1994, Regents of the University of California
  *
- * src/interfaces/ecpg/test/cmd_out_informix.c
+ * src/interfaces/ecpg/test/cmd_out.c
  *
  *-------------------------------------------------------------------------
  */
@@ -18,10 +18,8 @@
 
 // binary path is "../preproc/ecpg"
 
-#define EXEC_MODE_OPTION1 "-C"
-#define EXEC_MODE_OPTION2 "INFORMIX"
-#define PGC_FILE_NAME "preproc/notice_informix.pgc"
+#define PGC_FILE_NAME "preproc/notice.pgc"
 
 int main(void) {
-	return exec_ecpg(EXEC_MODE_OPTION1, EXEC_MODE_OPTION2, PGC_FILE_NAME, (char *)NULL);
+	return exec_ecpg(PGC_FILE_NAME, (char *)NULL);
 }
