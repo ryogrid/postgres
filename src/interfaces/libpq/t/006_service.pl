34f2195f3d6295c374b65f966f4b9c58ba09602a
Ryo Kanbayashi <ryo.contact@gmail.com>
2025-03-20 13:46:50 +0900

    apply perltidy.

--- src/interfaces/libpq/t/006_service.pl
+++ src/interfaces/libpq/t/006_service.pl
@@ -17,57 +17,66 @@ $node->init();
 # Start the PostgreSQL server
 $node->start();
 
-my $td = PostgreSQL::Test::Utils::tempdir;
+my $td      = PostgreSQL::Test::Utils::tempdir;
 my $srvfile = "$td/pgsrv.conf";
 
 # Create a service file
 open my $fh, '>', $srvfile or die $!;
 if ($windows_os) {
+
     # Windows: use CRLF
-    print $fh "[my_srv]", "\r\n";
-    print $fh join("\r\n", split(' ', $node->connstr)), "\r\n";
-} else {
+    print $fh "[my_srv]",                                   "\r\n";
+    print $fh join( "\r\n", split( ' ', $node->connstr ) ), "\r\n";
+}
+else {
     # Non-Windows: use LF
-    print $fh "[my_srv]", "\n";
-    print $fh join("\n", split(' ', $node->connstr)), "\n";
+    print $fh "[my_srv]",                                 "\n";
+    print $fh join( "\n", split( ' ', $node->connstr ) ), "\n";
 }
 close $fh;
 
 # Check that service option works as expected
 {
-	local $ENV{PGSERVICEFILE} = $srvfile;
-	$node->connect_ok(
-		'service=my_srv',
-		'service=my_srv',
-		sql => "SELECT 'connect1'",
-		expected_stdout => qr/connect1/);
+    local $ENV{PGSERVICEFILE} = $srvfile;
+    $node->connect_ok(
+        'service=my_srv',
+        'service=my_srv',
+        sql             => "SELECT 'connect1'",
+        expected_stdout => qr/connect1/
+    );
 
-	$node->connect_ok(
-		'postgres://?service=my_srv',
-		'postgres://?service=my_srv',
-		sql => "SELECT 'connect2'",
-		expected_stdout => qr/connect2/);
+    $node->connect_ok(
+        'postgres://?service=my_srv',
+        'postgres://?service=my_srv',
+        sql             => "SELECT 'connect2'",
+        expected_stdout => qr/connect2/
+    );
 
-	local $ENV{PGSERVICE} = 'my_srv';
-	$node->connect_ok(
-		'',
-		'envvar: PGSERVICE=my_srv',
-		sql => "SELECT 'connect3'",
-		expected_stdout => qr/connect3/);
+    local $ENV{PGSERVICE} = 'my_srv';
+    $node->connect_ok(
+        '',
+        'envvar: PGSERVICE=my_srv',
+        sql             => "SELECT 'connect3'",
+        expected_stdout => qr/connect3/
+    );
 }
 
 # Check that not existing service fails
 {
-	local $ENV{PGSERVICE} = 'non-existent-service';
-	$node->connect_fails(
-		'',
-		'envvar: PGSERVICE=non-existent-service',
-		expected_stdout => qr/definition of service "non-existent-service" not found/);
+    local $ENV{PGSERVICE} = 'non-existent-service';
+    $node->connect_fails(
+        '',
+        'envvar: PGSERVICE=non-existent-service',
+        expected_stdout =>
+          qr/definition of service "non-existent-service" not found/
+    );
 
-	$node->connect_fails(
-		'service=non-existent-service',
-		'service=non-existent-service',
-		expected_stderr => qr/definition of service "non-existent-service" not found/);
+    $node->connect_fails(
+        'service=non-existent-service',
+        'service=non-existent-service',
+        expected_stderr =>
+          qr/definition of service "non-existent-service" not found/
+    );
 }
 
 done_testing();
--- src/interfaces/libpq/t/007_servicefile_opt.pl
+++ src/interfaces/libpq/t/007_servicefile_opt.pl
@@ -17,32 +17,35 @@ $node->init();
 # Start the PostgreSQL server
 $node->start();
 
-my $td = PostgreSQL::Test::Utils::tempdir;
+my $td      = PostgreSQL::Test::Utils::tempdir;
 my $srvfile = "$td/pgsrv.conf";
 
 # Create a service file
 open my $fh, '>', $srvfile or die $!;
 if ($windows_os) {
+
     # Windows: use CRLF
-    print $fh "[my_srv]", "\r\n";
-    print $fh join("\r\n", split(' ', $node->connstr)), "\r\n";
+    print $fh "[my_srv]",                                   "\r\n";
+    print $fh join( "\r\n", split( ' ', $node->connstr ) ), "\r\n";
 
     # Escape backslashes for use in connection string later
     $srvfile =~ s/\\/\\\\/g;
-} else {
+}
+else {
     # Non-Windows: use LF
-    print $fh "[my_srv]", "\n";
-    print $fh join("\n", split(' ', $node->connstr)), "\n";
+    print $fh "[my_srv]",                                 "\n";
+    print $fh join( "\n", split( ' ', $node->connstr ) ), "\n";
 }
 close $fh;
 
 # Check that servicefile option works as expected
 {
     $node->connect_ok(
-        q{service=my_srv servicefile='}.$srvfile.q{'},
+        q{service=my_srv servicefile='} . $srvfile . q{'},
         'service=my_srv servicefile=...',
-        sql => "SELECT 'connect1'",
-        expected_stdout => qr/connect1/);
+        sql             => "SELECT 'connect1'",
+        expected_stdout => qr/connect1/
+    );
 
     # Escape slashes in servicefile path for use in connection string
     # Consider that the servicefile path may contain backslashes on Windows
@@ -54,39 +57,44 @@ close $fh;
     $encoded_srvfile =~ s/:/%3A/g;
 
     $node->connect_ok(
-        'postgresql:///?service=my_srv&servicefile='.$encoded_srvfile,
+        'postgresql:///?service=my_srv&servicefile=' . $encoded_srvfile,
         'postgresql:///?service=my_srv&servicefile=...',
-        sql => "SELECT 'connect2'",
-        expected_stdout => qr/connect2/);
+        sql             => "SELECT 'connect2'",
+        expected_stdout => qr/connect2/
+    );
 
     local $ENV{PGSERVICE} = 'my_srv';
     $node->connect_ok(
-        q{servicefile='}.$srvfile.q{'},
+        q{servicefile='} . $srvfile . q{'},
         'envvar: PGSERVICE=my_srv + servicefile=...',
-        sql => "SELECT 'connect3'",
-        expected_stdout => qr/connect3/);
+        sql             => "SELECT 'connect3'",
+        expected_stdout => qr/connect3/
+    );
 
     $node->connect_ok(
-        'postgresql://?servicefile='.$encoded_srvfile,
+        'postgresql://?servicefile=' . $encoded_srvfile,
         'envvar: PGSERVICE=my_srv + postgresql://?servicefile=...',
-        sql => "SELECT 'connect4'",
-        expected_stdout => qr/connect4/);
+        sql             => "SELECT 'connect4'",
+        expected_stdout => qr/connect4/
+    );
 }
 
 # Check that servicefile option takes precedence over PGSERVICEFILE environment variable
-{    
+{
     local $ENV{PGSERVICEFILE} = 'non-existent-file.conf';
 
     $node->connect_fails(
         'service=my_srv',
         'service=... fails with wrong PGSERVICEFILE',
-        expected_stderr => qr/service file "non-existent-file\.conf" not found/);
+        expected_stderr => qr/service file "non-existent-file\.conf" not found/
+    );
 
     $node->connect_ok(
-        q{service=my_srv servicefile='}.$srvfile.q{'},
+        q{service=my_srv servicefile='} . $srvfile . q{'},
         'servicefile= takes precedence over PGSERVICEFILE',
-        sql => "SELECT 'connect5'",
-        expected_stdout => qr/connect5/);
+        sql             => "SELECT 'connect5'",
+        expected_stdout => qr/connect5/
+    );
 }
 
 done_testing();
